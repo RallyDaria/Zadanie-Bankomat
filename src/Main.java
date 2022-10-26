c3b80822b99df05119ba4e376857e248ee0fc6e9
RallyDaria <dariazblewska@gmail.com>
2022-10-26 16:17:55 +0200

    bankomat

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        int kwota=10000;
+        Scanner sc=new Scanner(System.in);
+System.out.println("Obecne saldo to:"+ kwota+ "   Ile mam wypłacić");
+int wyplata=sc.nextInt();
+        System.out.println("Wypłaciłeś "+ wyplata + " Twoje dostępne saldo to: " + (kwota-wyplata));
     }
 }
